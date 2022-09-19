45c07c9ac1d2a0dc15f95d71ded3fdf213ca42b6
isaacbrds <isaacbrds>
2022-09-19 20:47:17 -0300

    Curso de VueJS - Router com Vuejs

--- router-guia/src/App.vue
+++ router-guia/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link :to="{name: 'isaac', params: {id: '20'} }">Isaac</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ router-guia/src/views/IsaacView.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>Formulário</h3>
+    <input type="text" v-model="id">
+    <input type="text" v-model="nome">
+    <button>Enviar</button>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id: '',
+            nome: ''
+        }
+    },
+    created(){
+        this.id = this.$route.params.id
+        if(this.id){
+            this.nome = 'Isaac'
+        }
+    }
+}
+</script>
