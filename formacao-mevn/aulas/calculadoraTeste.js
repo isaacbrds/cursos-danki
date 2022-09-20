a75c5e2ae03c946b51ef15848f617fd9033ecd1e
isaacbrds <isaac.brigido@gmail.com>
2022-09-20 10:24:49 -0300

    Formação MEVN - Aula 02

---
+++ formacao-mevn/aulas/calculadoraTeste.js
@@ -0,0 +1,9 @@
+const calculadora = require('./calculadora.js')
+
+console.log(`Somando usando calculadora: 3 + 4 = ${calculadora.soma(3,4)}`)
+
+console.log(`Subtraindo usando calculadora: 3 - 4 = ${calculadora.subtracao(3,4)}`)
+
+console.log(`Multiplicando usando calculadora: 3 X 4 = ${calculadora.multiplicacao(3,4)}`)
+
+console.log(`Dividindo usando calculadora: 3 / 4 = ${calculadora.divisao(3,4)}`)
